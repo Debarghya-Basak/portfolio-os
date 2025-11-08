e296b77fc0efb72921c218dbfb7e461ce46ca0f7
Debarghya Basak <106986982+Debarghya-Basak@users.noreply.github.com>
2025-11-09 00:03:55 +0530

    updated dock

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import "@/styles/styles.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserrat = Montserrat({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -20,11 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- components/apps/Dock/Dock.jsx
+++ components/apps/Dock/Dock.jsx
@@ -2,33 +2,79 @@
 
 import { useSelector } from "react-redux";
 import "./Dock.css";
-import { motion } from "framer-motion";
-
-const printDivs = () => {
-  const divs = [];
-
-  for (let i = 1; i <= 5; i++) {
-    divs.push(
-      <motion.div
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          duration: 0.8,
-          delay: i * 0.1,
-        }}
-        className="w-12 h-12 bg-gray-950 rounded-xl mr-4"
-      ></motion.div>
-    );
-  }
-
-  return <>{divs}</>;
-};
+import { delay, motion, scale } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const Dock = () => {
   const theme = useSelector((state) => state.theme.theme);
-
   console.log(theme);
+
+  //TIME CALCULATIONS
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (date) => {
+    return date.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+  };
+
+  const formatDate = (date) => {
+    return date.toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    });
+  };
+
+  const printDivs = () => {
+    const divs = [];
+
+    for (let i = 1; i <= 5; i++) {
+      divs.push(
+        <motion.div
+          key={i}
+          initial={{ y: 100, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              type: "spring",
+              duration: 0.8,
+              delay: i * 0.1,
+            },
+          }}
+          whileTap={{
+            scale: 1,
+            transition: {
+              type: "spring",
+              duration: 0.2,
+              delay: 0,
+            },
+          }}
+          whileHover={{
+            scale: 1.3,
+            transition: {
+              type: "spring",
+              duration: 0.4,
+              delay: 0,
+            },
+          }}
+          className="w-12 h-12 bg-gray-950 rounded-xl mr-4"
+        ></motion.div>
+      );
+    }
+
+    return <>{divs}</>;
+  };
 
   return (
     <motion.div
@@ -38,27 +84,20 @@ const Dock = () => {
         type: "spring",
         duration: 0.8,
       }}
-      className="fixed bottom-0 w-screen h-25 p-2.5"
+      className="fixed bottom-0 w-screen h-25 p-2.5 min-w-[1000]"
     >
       <div
         className={`theme ${
           theme === "dark" ? `dark` : ``
-        } w-full h-full rounded-2xl backdrop-blur-2xl p-4`}
+        } w-full h-full rounded-2xl backdrop-blur-2xl p-4 flex`}
       >
         <div className="app-container w-screen h-full flex items-center">
           {printDivs()}
-          {/* <motion.div
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-              type: "spring",
-              duration: 0.8,
-              delay: 0.2,
-            }}
-            className="w-12 h-12 bg-gray-950 rounded-xl"
-          ></motion.div> */}
         </div>
-        <div className="info-container"></div>
+        <div className="info-container w-25 h-full flex-col items-center justify-center">
+          <div className="w-full flex justify-center">{formatTime(time)}</div>
+          <div className="w-full flex justify-center">{formatDate(time)}</div>
+        </div>
       </div>
     </motion.div>
   );
